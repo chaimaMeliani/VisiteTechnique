7044ae72b060b15c696f59f127cdbd500f957307
chaima meliani <chaima.meliani@sesame.com.tn>
2018-12-18 14:16:37 +0100

    info client component

--- Front/src/app/info-client/info-client.component.ts
+++ Front/src/app/info-client/info-client.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-info-client',
   templateUrl: './info-client.component.html',
   styleUrls: ['./info-client.component.scss']
 })
 export class InfoClientComponent implements OnInit {
+  @ViewChild('addNewAppModal') addNewAppModal: ModalDirective;
   nomPrenom= null;
   cin= null;
   permis= null;
+  userName=null;
+  password=null;
   constructor() { 
     
   }
@@ -16,6 +19,14 @@ export class InfoClientComponent implements OnInit {
   ngOnInit() {
     this.nomPrenom="nom prenom";
     this.cin= "12345678";
-    this.permis= "987654321";  }
-
+    this.permis= "987654321";
+    this.userName="user name"; 
+  }
+  showModal(){
+    this.addNewAppModal.show();
+  }
+  submit(){
+    console.log(this.password);
+    this.addNewAppModal.hide();
+  }
 }
